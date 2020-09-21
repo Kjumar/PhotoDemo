58f4909044c24e5b7539d4e773ffc83b0c2641c6
Kjumar <jganguli@hotmail.ca>
2020-09-21 13:43:17 -0400

    added functionality from part 2 to Photo class

--- photo.js
+++ photo.js
@@ -1,9 +1,34 @@
 class Photo
 {
-    constructor(width, height)
+    constructor(width = 8, height = 10)
     {
         this.width = width;
         this.height = height;
     }
+
+    price()
+    {
+        if (this.width == 8 && this.height == 10)
+        {
+            return 4;
+        }
+        else if (this.width == 10 && this.height == 12)
+        {
+            return 6;
+        }
+        else
+        {
+            return 10;
+        }
+    }
+
+    toString()
+    {
+        return "This is a " + this.width + " by " + this.height + " photo and it costs " + this.price();
+    }
 }
 
+let p1 = new Photo();
+let p2 = new Photo(14, 10);
+console.log(p1.toString());
+console.log(p2.toString());
